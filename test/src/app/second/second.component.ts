5248ed2c77f14f7bd3903975c695fe62d23a119b
wajdbenhamed <wajdi.ben.hamed@gmail.com>
2023-12-22 23:23:53 +0100

    Initial commit

---
+++ test/src/app/second/second.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-second',
+  standalone: true,
+  imports: [],
+  templateUrl: './second.component.html',
+  styleUrl: './second.component.css'
+})
+export class SecondComponent {
+
+}
